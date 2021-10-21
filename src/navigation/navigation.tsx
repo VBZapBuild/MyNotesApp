02788936b93baa8ca5a5678e247bdc310211615a
Vibhas Behl <91947913+VBZapBuild@users.noreply.github.com>
2021-10-21 08:55:27 -0700

    initial commit

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListScreen from '../screens/ListScreen';
+import DetailScreen from '../screens/DetailScreen';
+
+export type RootStackParamList = {
+    list: undefined,
+    detail: 
+    {
+        data: any,
+        add :any
+    }
+  };
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}>
+                
+                <Stack.Screen name="list" component={ListScreen} />
+                <Stack.Screen name="detail" component={DetailScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
