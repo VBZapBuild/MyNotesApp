6bed4d105684f92f2dc9cf950f08e9947b08a5de
Vibhas Behl <91947913+VBZapBuild@users.noreply.github.com>
2021-11-02 12:51:16 -0700

    counterScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import Navigation from './src/navigation/navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListScreen from './src/screens/ListScreen';
+import DetailScreen from './src/screens/DetailScreen';
 import {Provider as StoreProvider} from 'react-redux';
+import CounterScreen from './src/screens/CounterScreen';
+
+export type RootStackParamList = {
+  list: undefined,
+  detail: 
+  {
+      data: any,
+      add :any
+  }
+};
 import store from './src/reducer/store';
+const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
-    <StoreProvider store = {store}>
-    <Navigation></Navigation>
-    </StoreProvider>
+    // <StoreProvider store = {store}>
+    // {/* <Navigation></Navigation> */}
+    // <NavigationContainer>
+    //         <Stack.Navigator
+    //             screenOptions={{
+    //                 headerShown: false
+    //             }}>
+                
+    //             <Stack.Screen name="list" component={ListScreen} />
+    //             <Stack.Screen name="detail" component={DetailScreen} />
+    //         </Stack.Navigator>
+    //     </NavigationContainer>
+    // </StoreProvider>
+    <CounterScreen/>
   );
 }
